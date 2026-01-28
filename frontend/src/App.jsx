b1b514297f071e7cd8338eff878d4004ddf59f72
janidu98 <janinduchathushan@gmail.com>
2026-01-28 22:50:03 +0530

    Trigger frontend CI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,11 +8,13 @@ const App = () => {
   return (
     <div className="relative w-full h-full">
       <div className="absolute inset-0 -z-10 w-full h-full items-center px-5 py-4 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]">
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreateNote />} />
           <Route path="/note/:id" element={<NoteDetails />} />
         </Routes>
+        
       </div>
     </div>
   );
